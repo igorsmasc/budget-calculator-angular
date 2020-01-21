78589f4d655c9081484ecb5452738bf04628b2a3
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2020-01-21 14:29:50 -0300

    adding and removing items

--- src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { BudgetItem } from 'src/shared/models/budged-item.model';
 
 @Component({
   selector: 'app-budget-item-card',
@@ -7,11 +8,17 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class BudgetItemCardComponent implements OnInit {
 
-  @Input() isIncome: boolean = true;
+  @Input() item: BudgetItem;
+  @Output() xButtonClick: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onXButtonClick() {
+    // here we wat emit an event
+    this.xButtonClick.emit();
   }
 
 }
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BudgetItem } from 'src/shared/models/budged-item.model';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
+  budgetItems: BudgetItem[] = new Array<BudgetItem>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addItem(newItem: BudgetItem) {
+    this.budgetItems.push(newItem);
+  }
+
+  deleteItem(item: BudgetItem) {
+    const index = this.budgetItems.indexOf(item);
+
+    this.budgetItems.splice(index, 1);
   }
 
 }
